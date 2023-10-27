be6788a8fd27e4b49a2fdefcf4f9ceb20c2fdfc6
Dovydas Štrupaitis <45108602+ValIIDev@users.noreply.github.com>
2023-10-27 19:49:06 +0300

    Revert "Added basic code to convert centimeters to inches"

--- Metric_Converter/Form1.cs
+++ Metric_Converter/Form1.cs
@@ -7,79 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Resources.ResXFileRef;
-
 
 namespace Metric_Converter
 {
-
     public partial class Form1 : Form
     {
-
-        private Cm_inchConvert converter; // Define the Cm_inchConvert object at a scope accessible to both event handlers 
-        class Cm_inchConvert
-        {
-            private double ValueInCentimeters;
-            private double ValueInInches;
-
-            public Cm_inchConvert(double valueInCentimeters)
-            {
-                ValueInCentimeters = valueInCentimeters;
-                ConvertToInches();
-            }
-
-            public Cm_inchConvert(double valueInInches, bool inches)
-            {
-                ValueInInches = valueInInches;
-                ConvertToCentimeters();
-            }
-
-            public double GetValueInCentimeters()
-            {
-                return ValueInCentimeters;
-            }
-
-            public double GetValueInInches()
-            {
-                return ValueInInches;
-            }
-
-            private void ConvertToInches()
-            {
-                ValueInInches = ValueInCentimeters / 2.54; // 1 inch = 2.54 cm
-            }
-
-            private void ConvertToCentimeters()
-            {
-                ValueInCentimeters = ValueInInches * 2.54; // 1 inch = 2.54 cm
-            }
-        }
-
         public Form1()
         {
             InitializeComponent();
-
         }
-
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (double.TryParse(textBox1.Text, out double centimeters))
-            {
-                converter = new Cm_inchConvert(centimeters); // Create the converter instance with the user input in centimeters
-                double inchesValue = converter.GetValueInInches(); // Get the value in inches
-                textBox2.Text = inchesValue.ToString();
-            }
-        }
-
-
-
-
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
-
     }
 }
